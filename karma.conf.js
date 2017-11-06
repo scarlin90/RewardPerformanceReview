9fb1e74f2e6e60d3eaeb4b031a94a9d04ff3fc53
scarlin90 <scarlin90@github.com>
2017-11-06 11:42:05 +0000

    Adding no-sandbox flag to angular chrome tests for container

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      Chrome_no_sandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox'] // for running within Docker
+      }
+    },
     singleRun: false
   });
 };
