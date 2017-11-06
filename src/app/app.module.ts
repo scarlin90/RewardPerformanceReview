072d2b3b44ffbf33651ebe3ff543b59057e42cb7
scarlin90 <scarlin90@github.com>
2017-11-06 12:23:51 +0000

    Added in HttpModule as import to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpService } from './services/index';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   providers: [HttpService],
+  imports: [
+    HttpModule,
+    SharedModule,
+  ],
 })
 export class SharedModule { }
